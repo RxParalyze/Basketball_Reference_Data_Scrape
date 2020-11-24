a87325aef2c5f88d609d25980ba21654b9b18832
RxParalyze <c17phillip.smart@gmail.com>
2020-11-24 15:59:08 -0500

    corrected sql file

--- lineups.sql
+++ lineups.sql
@@ -1,86 +1,81 @@
-DROP DATABASE IF EXISTS lineup_db;
-CREATE DATABASE lineup_db;
-USE lineup_db;
-CREATE TABLE lineups (
-  lineup_rank INT COLLATE utf8_unicode_ci NOT NULL,
-  player1_lineup VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
-  player2_lineup VARCHAR(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  game_date DATE COLLATE utf8_unicode_ci DEFAULT NULL,
-  lineup_team VARCHAR(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  home_or_away VARCHAR(3) COLLATE utf8_unicode_ci DEFAULT NULL,
-  opp_team VARCHAR(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  win_or_loss VARCHAR(3) COLLATE utf8_unicode_ci DEFAULT NULL,
-  minutes_played DECIMAL COLLATE utf8_unicode_ci NOT NULL,
-  possessions_team1 INT COLLATE utf8_unicode_ci NOT NULL,
-  possessions_team2 INT COLLATE utf8_unicode_ci NOT NULL,
-  pace_factor DECIMAL COLLATE utf8_unicode_ci NOT NULL,
-  field_goals INT COLLATE utf8_unicode_ci NOT NULL,
-  field_goal_attempts INT COLLATE utf8_unicode_ci NOT NULL,
-  field_goal_perc DECIMAL COLLATE utf8_unicode_ci DEFAULT NULL,
-  three_pt_fgs INT COLLATE utf8_unicode_ci NOT NULL,
-  three_pt_fg_attempts INT COLLATE utf8_unicode_ci NOT NULL,
-  three_pt_fg_perc DECIMAL COLLATE utf8_unicode_ci NOT NULL,
-  effective_fg_perc DECIMAL COLLATE utf8_unicode_ci NOT NULL,
-  free_throws INT COLLATE utf8_unicode_ci NOT NULL,
-  free_throw_attempts INT COLLATE utf8_unicode_ci NOT NULL,
-  free_throw_perc DECIMAL COLLATE utf8_unicode_ci DEFAULT NULL,
-  points INT COLLATE utf8_unicode_ci NOT NULL,
+DROP SCHEMA IF EXISTS "lineup_schema" CASCADE;
+CREATE SCHEMA "lineup_schema";
+SET search_path TO "lineup_schema";
+
+CREATE TABLE "teams" (
+  team_name VARCHAR(255)  NOT NULL,
+  game_numbers INT ARRAY,
+  players VARCHAR ARRAY,
+  PRIMARY KEY (team_name)
+) ;
+
+CREATE TABLE "players" (
+  player_name VARCHAR(255)  NOT NULL,
+  player_lineups INTEGER ARRAY  NOT NULL,
+  team_name VARCHAR(255)  DEFAULT NULL,
+  PRIMARY KEY (player_name),
+  FOREIGN KEY (team_name) REFERENCES teams(team_name)
+) ;
+
+CREATE TABLE "games" (
+  game_number SERIAL,
+  home_team VARCHAR(255)  NOT NULL,
+  away_team VARCHAR(255)  NOT NULL,
+  game_date DATE,
+  winning_team VARCHAR(255)  NOT NULL,
+  PRIMARY KEY (game_number),
+  FOREIGN KEY (home_team) REFERENCES teams(team_name),
+  FOREIGN KEY (away_team) REFERENCES teams(team_name)
+);
+
+CREATE TABLE "lineups" (
+  lineup_rank INT  NOT NULL,
+  player1 VARCHAR(255)  NOT NULL,
+  player2 VARCHAR(255)  DEFAULT NULL,
+  game_number INT  NOT NULL,
+  pace_factor DECIMAL  NOT NULL,
+  points INT  NOT NULL,
   pull_time TIMESTAMP DEFAULT NOW(),
-  url TEXT COLLATE utf8_unicode_ci DEFAULT NULL,
+  url TEXT  DEFAULT NULL,
   PRIMARY KEY (lineup_rank),
-  FOREIGN KEY (player1) REFERENCES players(player_name)
-) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
+  FOREIGN KEY (player1) REFERENCES players(player_name),
+  FOREIGN KEY (player2) REFERENCES players(player_name),
+  FOREIGN KEY (game_number) REFERENCES games(game_number)
+) ;
 
-CREATE TABLE players (
-  player_name VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
-  player_lineups INTEGER ARRAY COLLATE utf8_unicode_ci NOT NULL,
-  game_date DATE COLLATE utf8_unicode_ci DEFAULT NULL,
-  lineup_team VARCHAR(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  minutes_played DECIMAL COLLATE utf8_unicode_ci NOT NULL,
-  possessions_team1 INT COLLATE utf8_unicode_ci NOT NULL,
-  possessions_team2 INT COLLATE utf8_unicode_ci NOT NULL,
-  pace_factor DECIMAL COLLATE utf8_unicode_ci NOT NULL,
-
-  points INT COLLATE utf8_unicode_ci NOT NULL,
-  pull_time TIMESTAMP DEFAULT NOW(),
-  url TEXT COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (lineup_rank),
-  FOREIGN KEY (lineup_rank) REFERENCES players(lineup_rank)
-) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
-
-CREATE TABLE field_goals (
-  field_goals INT COLLATE utf8_unicode_ci NOT NULL,
-  field_goal_attempts INT COLLATE utf8_unicode_ci NOT NULL,
-  field_goal_perc DECIMAL COLLATE utf8_unicode_ci DEFAULT NULL,
+CREATE TABLE "field_goals" (
+  lineup_rank INT  NOT NULL,
+  field_goals INT  NOT NULL,
+  field_goal_attempts INT  NOT NULL,
+  field_goal_perc DECIMAL  DEFAULT NULL,
   FOREIGN KEY (lineup_rank) REFERENCES lineups(lineup_rank)
-) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
+) ;
 
-CREATE TABLE possessions (
+CREATE TABLE "possessions" (
+  lineup_rank INT  NOT NULL,
+  possessions_team1 INT  NOT NULL,
+  possessions_team2 INT  NOT NULL,
   FOREIGN KEY (lineup_rank) REFERENCES lineups(lineup_rank)
-) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
+) ;
 
-CREATE TABLE three_pt_fgs (
-  three_pt_fgs INT COLLATE utf8_unicode_ci NOT NULL,
-  three_pt_fg_attempts INT COLLATE utf8_unicode_ci NOT NULL,
-  three_pt_fg_perc DECIMAL COLLATE utf8_unicode_ci NOT NULL,
-  effective_fg_perc DECIMAL COLLATE utf8_unicode_ci NOT NULL,
+CREATE TABLE "three_pt_fgs" (
+  lineup_rank INT  NOT NULL,
+  three_pt_fgs INT  NOT NULL,
+  three_pt_fg_attempts INT  NOT NULL,
+  three_pt_fg_perc DECIMAL  NOT NULL,
+  effective_fg_perc DECIMAL  NOT NULL,
   FOREIGN KEY (lineup_rank) REFERENCES lineups(lineup_rank)
-) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
+) ;
 
-CREATE TABLE free_throws (
-  free_throws INT COLLATE utf8_unicode_ci NOT NULL,
-  free_throw_attempts INT COLLATE utf8_unicode_ci NOT NULL,
-  free_throw_perc DECIMAL COLLATE utf8_unicode_ci DEFAULT NULL,
+CREATE TABLE "free_throws" (
+  lineup_rank INT  NOT NULL,
+  free_throws INT  NOT NULL,
+  free_throw_attempts INT  NOT NULL,
+  free_throw_perc DECIMAL  DEFAULT NULL,
   FOREIGN KEY (lineup_rank) REFERENCES lineups(lineup_rank)
-) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
+) ;
 
-CREATE TABLE teams (
-  FOREIGN KEY (lineup_rank) REFERENCES lineups(lineup_rank)
-) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
 
-CREATE TABLE location_stats (
-  FOREIGN KEY (lineup_rank) REFERENCES lineups(lineup_rank)
-) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
 
-CREATE TABLE games ();
+
 
---
+++ runpgadmin.sh
@@ -0,0 +1,3 @@
+cd
+cd /.local/lib/python3.8/site-packages/pgadmin4
+sudo python3 pgadmin4.py
